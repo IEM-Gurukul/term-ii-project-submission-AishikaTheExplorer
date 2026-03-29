f1295f6493462f826e43214e39a1ecd7181308ac
AishikaTheExplorer <aishika2316@gmail.com>
2026-03-29 21:52:29 +0530

    order class was created

---
+++ src/model/Order.java
@@ -0,0 +1,34 @@
+package model;
+
+// Abstract class = Abstraction
+public abstract class Order {
+    protected int orderId;
+    protected String customerName;
+    protected String address;
+    protected double distance;
+    protected int priority;
+    protected String status;
+
+    public Order(int orderId, String customerName, String address, double distance) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.address = address;
+        this.distance = distance;
+        this.status = "Pending";
+    }
+
+    // Polymorphism (overridden in subclasses)
+    public abstract int calculatePriority();
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDetails() {
+        return orderId + " - " + customerName + " (" + status + ")";
+    }
+}
